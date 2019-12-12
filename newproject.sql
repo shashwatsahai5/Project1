0a4119f7350355a83cdcf3b1f482d99514d06399
shashwatsahai5 <37666561+shashwatsahai5@users.noreply.github.com>
2019-12-12 17:52:36 +0530

    Add files via upload

---
+++ newproject.sql
@@ -0,0 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 12, 2019 at 01:20 PM
+-- Server version: 10.1.39-MariaDB
+-- PHP Version: 7.3.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `newproject`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(5) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phno` varchar(20) NOT NULL,
+  `sex` varchar(20) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `hash` varchar(50) NOT NULL,
+  `active` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `phno`, `sex`, `address`, `password`, `hash`, `active`) VALUES
+(1, 'shashwat sahai', 'shashwat@gmail.com', '817836513', 'M', '123 delhi 6', '12345', '', 1),
+(2, 'abc', 'abc@gmail.com', '9899272102', 'M', 'Vellore, India', '123456', '', 1),
+(3, 'sharad', 'sharad@gmail.com', '9999887744', 'Male', 'gurgaon, haryana', 'Shashwat@123', '7939a186137a882ce40165f6e48bf283', 0),
+(4, 'maunil nakarani', 'shashwatsahai5@gmail.com', '8178365138', 'Male', 'Vadodara, Gujrat', 'Shashwat@123', '7939a186137a882ce40165f6e48bf283', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
